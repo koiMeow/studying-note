330b08b5c4e92e51c5c0cf2b2349a36119234481
koiMeow <ja930036@gmail.com>
2024-07-02 19:32:43 +0800

    Edit constants.js: API_HOST

--- src/global/constants.js
+++ src/global/constants.js
@@ -1,3 +1,4 @@
-export const API_HOST = "http://localhost:3000"
+export const API_HOST = "https://studying-note.vercel.app/"
+export const API_LOCAL_HOST = "http://localhost:3000"
 export const API_GET_DATA = `${API_HOST}/notes/1`
 export const CATEGORIES = ["React", "Python", "C/C++", "C#"]
